38196997500b60de4339bffa433e5cbc13a68a74
Oliver <Test@googlemail.com>
2016-12-23 17:18:39 +0000

    db stuff

--- Database.sql
+++ Database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.17
--- https://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: mysql.dur.ac.uk
--- Generation Time: Nov 07, 2016 at 04:11 PM
--- Server version: 5.1.39-community-log
--- PHP Version: 5.3.27
+-- Host: 127.0.0.1
+-- Generation Time: Dec 23, 2016 at 06:18 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,31 +14,44 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Xdnfn65_Database`
+-- Database: `gpmain`
 --
-
--- ------------------------*NOTES*-------------------------
-
--- Need to setup relationships between entries but it's best to do this once the structure fully takes shape
-
--- Setup automatic pruning of old entries using maintenance scripts, possibly configurable through an admin account
-
--- Autonum for userID and foodID
-
--- Change text sizes in itemTable. Category is smaller than description, no idea how to do that in this file.
-
--- Made manual edits to this and it might not work, can always roll back
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `itemTable`
+-- Table structure for table `authtable`
 --
 
-CREATE TABLE IF NOT EXISTS `itemTable` (
+CREATE TABLE `authtable` (
+  `userid` int(11) NOT NULL,
+  `authToken` text NOT NULL,
+  `expirDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chattable`
+--
+
+CREATE TABLE `chattable` (
+  `user1` int(11) NOT NULL,
+  `user2` int(11) NOT NULL,
+  `user1seen` timestamp NULL DEFAULT NULL,
+  `user2seen` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemtable`
+--
+
+CREATE TABLE `itemtable` (
   `foodid` int(11) NOT NULL,
   `expirydate` date NOT NULL,
   `category` text NOT NULL,
@@ -47,134 +60,180 @@ CREATE TABLE IF NOT EXISTS `itemTable` (
   `description` text NOT NULL,
   `location` text NOT NULL,
   `amount` int(11) NOT NULL,
-  `weight` FLOAT NOT NULL, 
+  `weight` float NOT NULL,
   `image` text NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `hidden` tinyint(1) NOT NULL,  
-  PRIMARY KEY (`foodid`)
+  `hidden` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messageTable`
+-- Table structure for table `messagetable`
 --
 
-CREATE TABLE IF NOT EXISTS `messageTable` (
+CREATE TABLE `messagetable` (
   `messageid` int(11) NOT NULL,
   `message` text NOT NULL,
-  `time` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`messageid`)
+  `time` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `userMessageTable`
--- FK to messageTable
+-- Table structure for table `requestmessagetable`
 --
 
-CREATE TABLE IF NOT EXISTS `userMessageTable` (
+CREATE TABLE `requestmessagetable` (
   `messageid` int(11) NOT NULL,
   `sender` int(11) NOT NULL,
-  `receiver` int(11) NOT NULL,
-  PRIMARY KEY (`messageid`)
+  `requestid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `requestMessageTable`
--- FK to messageTable
+-- Table structure for table `requesttable`
 --
 
-CREATE TABLE IF NOT EXISTS `requestMessageTable` (
-  `messageid` int(11) NOT NULL,
-  `sender` int(11) NOT NULL,
-  `requestid` int(11) NOT NULL,
-  PRIMARY KEY (`messageid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
-/* If this seems like overkill, let me explain.
- 
-Storing seen for each message can be a huge amount of data and is very liable to data corruption.
-It may seem like storing this new table will take up a lot of space but it shouldn't be too large.
-
-In return we get to just check the user's lastseen against the time of the message and don't get any database oddities.
-Lookup times checking integers against integers is crazy fast. You could easily search 1 million entries in less than 50ms,
-so receiving the entries form the chatTable won't be ap roblem
-
-We can prune the database and archieve/delete old messages and chatTable entries using automatic scripts 
-that arn't hard to setup, especially with this config, that will keep the DB size reasonable.
-
-We can also prune all messages related to requests that are deleted instantly, and ones that are completed after a set time
-
-*/
-
--- --------------------------------------------------------
-
---
--- Table structure for table `chatTable`
---
-
-CREATE TABLE IF NOT EXISTS `chatTable` (
-  `user1` int(11) NOT NULL,
-  `user2` int(11) NOT NULL,
-  `user1seen` TIMESTAMP,
-  `user2seen` TIMESTAMP,
-  PRIMARY KEY (`user1`,`user2`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `requestTable`
---
-
-CREATE TABLE IF NOT EXISTS `requestTable` (
+CREATE TABLE `requesttable` (
   `requestid` int(11) NOT NULL,
   `requester` int(11) NOT NULL,
   `foodid` int(11) NOT NULL,
-  `accepted` tinyint(1) NOT NULL,  
-  PRIMARY KEY (`requestid`)
+  `accepted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userTable`
+-- Table structure for table `usermessagetable`
 --
 
-CREATE TABLE IF NOT EXISTS `userTable` (
+CREATE TABLE `usermessagetable` (
+  `messageid` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertable`
+--
+
+CREATE TABLE `usertable` (
   `userid` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `picture` text,
-  `email` text,
-  PRIMARY KEY (`userid`)
+  `email` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 --
--- Table structure for table `authTable`
--- Holds authentication tokens so a user may stay logged in
--- We ask for username and password once and then exclusively use auth tokens
--- This means users can never spoof the userID being sent to request data and must always use their auth token
--- Stops problems such as a coder not realising and accepting any userID in say a message request function
--- That lets someone read anyone's messages. It can happen anytime and not having to validate input on every little thing
--- Is real nice
+-- Indexes for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `authTable` (
-  `userid` int(11) NOT NULL,
-  `authToken` text NOT NULL,
-  `expirDate` DATE NOT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `authtable`
+--
+ALTER TABLE `authtable`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `chattable`
+--
+ALTER TABLE `chattable`
+  ADD PRIMARY KEY (`user1`,`user2`),
+  ADD KEY `user2` (`user2`);
+
+--
+-- Indexes for table `itemtable`
+--
+ALTER TABLE `itemtable`
+  ADD PRIMARY KEY (`foodid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `messagetable`
+--
+ALTER TABLE `messagetable`
+  ADD PRIMARY KEY (`messageid`);
+
+--
+-- Indexes for table `requestmessagetable`
+--
+ALTER TABLE `requestmessagetable`
+  ADD PRIMARY KEY (`messageid`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `requestid` (`requestid`);
+
+--
+-- Indexes for table `requesttable`
+--
+ALTER TABLE `requesttable`
+  ADD PRIMARY KEY (`requestid`),
+  ADD KEY `requester` (`requester`),
+  ADD KEY `foodid` (`foodid`);
+
+--
+-- Indexes for table `usermessagetable`
+--
+ALTER TABLE `usermessagetable`
+  ADD PRIMARY KEY (`messageid`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`);
+
+--
+-- Indexes for table `usertable`
+--
+ALTER TABLE `usertable`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `authtable`
+--
+ALTER TABLE `authtable`
+  ADD CONSTRAINT `authtable_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `chattable`
+--
+ALTER TABLE `chattable`
+  ADD CONSTRAINT `chattable_ibfk_1` FOREIGN KEY (`user1`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chattable_ibfk_2` FOREIGN KEY (`user2`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `itemtable`
+--
+ALTER TABLE `itemtable`
+  ADD CONSTRAINT `itemtable_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `requestmessagetable`
+--
+ALTER TABLE `requestmessagetable`
+  ADD CONSTRAINT `requestmessagetable_ibfk_1` FOREIGN KEY (`messageid`) REFERENCES `messagetable` (`messageid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `requestmessagetable_ibfk_2` FOREIGN KEY (`sender`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `requestmessagetable_ibfk_3` FOREIGN KEY (`requestid`) REFERENCES `requesttable` (`requestid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `requesttable`
+--
+ALTER TABLE `requesttable`
+  ADD CONSTRAINT `requesttable_ibfk_1` FOREIGN KEY (`requester`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `requesttable_ibfk_2` FOREIGN KEY (`foodid`) REFERENCES `itemtable` (`foodid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `usermessagetable`
+--
+ALTER TABLE `usermessagetable`
+  ADD CONSTRAINT `usermessagetable_ibfk_1` FOREIGN KEY (`messageid`) REFERENCES `messagetable` (`messageid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usermessagetable_ibfk_2` FOREIGN KEY (`sender`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usermessagetable_ibfk_3` FOREIGN KEY (`receiver`) REFERENCES `usertable` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- GPMain/database.php
+++ GPMain/database.php
@@ -1,37 +1,55 @@
 <?php
-/** @var PDO $conn */
-$conn = NULL;
+function getPDO() {
+    $servername = "mysql: host=127.0.0.1;dbname=gpmain;port=3306";
+    $username = "gptest";
+    $password = "123";
 
-function connectDB() {
-    global $conn;
-    if($conn == null){
-        $servername = "mysql: host=127.0.0.1;dbname=gpmain;port=3306";
-        $username = "gpmain";
-        $password = "123";
-
-        // Create connection
-        try {
-            $conn = new PDO($servername, $username, $password,
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true));
-        } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
-            return false;
-        }
-
-    }
-    return true;
-}
-
-function execQuery($inputStr, $params) {
-    global $conn;
-    $result = NULL;
-
-    if (($conn != null) || connectDB()) {
-        $result = $conn->prepare($inputStr);
-        $result->execute($params);
-    } else {
+    // Create connection
+    try {
+        $conn = new PDO($servername, $username, $password,
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true));
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
         return NULL;
     }
 
-    return $result;
+    return $conn;
 }
+
+class MySQL_DataMapper
+{
+    private $table = 'gpmain';
+
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function fetchUserById($id)
+    {
+        $query = "SELECT * FROM `{$this->table}` WHERE `id` =:id";
+        $stmt = $this->pdo->prepare($query);
+
+        $stmt->execute(array(
+            ':id' => $id
+        ));
+
+        return $stmt->fetch();
+    }
+}
+
+//function execQuery($inputStr, $params) {
+//    global $conn;
+//    $result = NULL;
+//
+//    if (($conn != null) || connectDB()) {
+//        $result = $conn->prepare($inputStr);
+//        $result->execute($params);
+//    } else {
+//        return NULL;
+//    }
+//
+//    return $result;
+//}
