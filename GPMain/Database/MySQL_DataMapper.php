37871d8b01719f908041845c43486e504a8c8470
Oliver <Test@googlemail.com>
2016-12-24 14:21:20 +0000

    DFD

--- GPMain/Database/MySQL_DataMapper.php
+++ GPMain/Database/MySQL_DataMapper.php
@@ -3,7 +3,7 @@
 class MySQL_DataMapper
 {
 
-    private $userstable = 'usertable';
+    //private $userstable = 'usertable';
 
     /** @var PDO pdo */
     private $pdo;
@@ -13,20 +13,27 @@ class MySQL_DataMapper
         $this->pdo = $pdo;
     }
 
-    public function fetchUserById($id)
+    //Never call directly, simply inserts values
+    public function addNewUser($un,$pw,$pic,$email)
     {
-        $query = "INSERT INTO `{$this->userstable}` (username)";
+        $query = "INSERT INTO usertable (username, password, picture, email) VALUES (:un, :pw, :pic, :email)";
+        $result = true;
         try {
             $stmt = $this->pdo->prepare($query);
 
             $stmt->execute(array(
-                ':id' => $id
+                ':un' => $un,
+                ':pw' => $pw,
+                ':pic' => $pic,
+                ':email' => $email
             ));
         } catch (PDOException $e) {
-            if (DEBUG) echo 'Fetch failed: ' . $e->getMessage();
-            return NULL;
+            if (DEBUG) echo 'Adding new user failed: ' . $e->getMessage();
+            $result = false;
         }
-
-        return $stmt->fetch();
+        $stmt = NULL;
+        return $result;
     }
+
+
 }
