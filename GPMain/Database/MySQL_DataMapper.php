58988fb3d27002c78e4a5c8290d285ac80490266
Oliver <Test@googlemail.com>
2016-12-24 17:32:06 +0000

    Data wrapper for basic functionallity

--- GPMain/Database/MySQL_DataMapper.php
+++ GPMain/Database/MySQL_DataMapper.php
@@ -3,8 +3,6 @@
 class MySQL_DataMapper
 {
 
-    //private $userstable = 'usertable';
-
     /** @var PDO pdo */
     private $pdo;
 
@@ -13,7 +11,7 @@ class MySQL_DataMapper
         $this->pdo = $pdo;
     }
 
-    //Never call directly, simply inserts values
+    //Never call directly, simply inserts values. Use request handler
     public function addNewUser($un,$pw,$pic,$email)
     {
         $query = "INSERT INTO usertable (username, password, picture, email) VALUES (:un, :pw, :pic, :email)";
@@ -35,5 +33,161 @@ class MySQL_DataMapper
         return $result;
     }
 
+    public function addNewUserMessage($message, $sender, $receiver)
+    {
+        $query = "INSERT INTO messagetable (message, time) VALUES (:msg, NOW());";
+        $query .= "INSERT INTO usermessagetable (messageid, sender, receiver) VALUES (LAST_INSERT_ID(), :send, :rec)";
+        $result = true;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':msg' => $message,
+                ':send' => $sender,
+                ':rec' => $receiver
+            ));
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Adding new user failed: ' . $e->getMessage();
+            $result = false;
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    public function addNewFoodItem() //TODO
+    {
+        return false;
+    }
+
+    public function getUserMessagesByID($id)
+    {
+        $query = "SELECT `messagetable`.`message`,`messagetable`.`time`, `usermessagetable`.`sender`, `usermessagetable`.`receiver`
+                    FROM `messagetable`
+                        INNER JOIN `usermessagetable`
+                        ON `messagetable`.`messageid` = `usermessagetable`.`messageid`
+                    WHERE (`sender` = :id) OR (`receiver` = :id)";
+        $result = NULL;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':id' => $id
+            ));
+
+            $result = $stmt->fetchAll();
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Get user messages failed: ' . $e->getMessage();
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    public function getAuthTokenByID($id)
+    {
+        $query = "SELECT `authToken`, `expirDate`
+                    FROM `authtable`
+                    WHERE `userid` = :id";
+        $result = NULL;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':id' => $id
+            ));
+
+            $result = $stmt->fetchAll();
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Getting auth token failed: ' . $e->getMessage();
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    public function getPasswordByID($id)
+    {
+        $query = "SELECT `password`
+                    FROM `usertable`
+                    WHERE `userid` = :id";
+        $result = NULL;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':id' => $id
+            ));
+
+            $result = $stmt->fetchAll();
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Getting password failed: ' . $e->getMessage();
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    public function getFoodItemsByUserID($id) // TODO
+    {
+
+    }
+
+    public function addNewRequest($requester, $foodid)
+    {
+        $query = "INSERT INTO requesttable (requester, foodid) VALUES (:req, :food)";
+        $result = true;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':req' => $requester,
+                ':food' => $foodid
+            ));
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Adding new user failed: ' . $e->getMessage();
+            $result = false;
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    public function addNewRequestMessage($message, $sender, $requestID)
+    {
+        $query = "INSERT INTO messagetable (message, time) VALUES (:msg, NOW());";
+        $query .= "INSERT INTO requestmessagetable (messageid, sender, requestid) VALUES (LAST_INSERT_ID(), :send, :reqid)";
+        $result = true;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':msg' => $message,
+                ':send' => $sender,
+                ':reqid' => $requestID
+            ));
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Adding new user failed: ' . $e->getMessage();
+            $result = false;
+        }
+        $stmt = NULL;
+        return $result;
+    }
+
+    //Instate as boolean
+    public function setRequestState($requestid, $instate)
+    {
+        $state = $instate ? 1 : 0;
+        $query = "UPDATE `requesttable` SET `accepted` = :state WHERE `requestid` = :reqid";
+        $result = true;
+        try {
+            $stmt = $this->pdo->prepare($query);
+
+            $stmt->execute(array(
+                ':reqid' => $requestid,
+                ':state' => $state
+            ));
+        } catch (PDOException $e) {
+            if (DEBUG) echo 'Adding new user failed: ' . $e->getMessage();
+            $result = false;
+        }
+        $stmt = NULL;
+        return $result;
+    }
 
 }
