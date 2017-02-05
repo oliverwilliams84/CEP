ec109764e272ef645ef67716c80f01a46a6ab48a
Oliver <Test@googlemail.com>
2017-02-05 20:48:05 +0000

    food maybe ready. images need testing

--- GPMain/handlers/food.php
+++ GPMain/handlers/food.php
@@ -27,23 +27,80 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
 //HANDLE POST
 elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $topic = $_POST["topic"];
-    $question = $_POST["question"];
-    $answer = $_POST["answer"];
-    $authToken = $_POST["auth_token"];
+    $name = $_POST["name"];
+    $expirDate = $_POST["expiredate"];
+    $category = $_POST["category"];
+    $userID = $_POST["userid"];
+    $desc = $_POST["description"];
+    $lat = $_POST["laitutde"];
+    $long = $_POST["longitude"];
+    $amount = $_POST["amount"];
+    $weight = $_POST["weight"];
+    $imagedir = null;
 
     //Check Vars
-    if (!is_numeric($topic)){
-        die(json_encode(array("error" => "topic incorrectly defined")));
-    } elseif (!is_string($question)) {
-        die(json_encode(array("error" => "question incorrect defined")));
-    } elseif (!is_string($answer)) {
-        die(json_encode(array("error" => "answer incorrect defined")));
-    } elseif (!is_string($authToken) || !checkAuthToken($authToken)) {
-        die(json_encode(array("error" => "not authorised")));
+    if (!is_numeric($userID)){
+        die(json_encode(array("error" => "userID incorrectly defined")));
+    } elseif (!is_string($name)) {
+        die(json_encode(array("error" => "name incorrectly defined")));
+    } elseif (!is_string($expirDate)) {
+        die(json_encode(array("error" => "expirey incorrectly defined")));
+    } elseif (!is_string($category)) {
+        die(json_encode(array("error" => "category incorrectly defined")));
+    } elseif (!is_string($desc)) {
+        die(json_encode(array("error" => "description incorrectly defined")));
+    } elseif (!is_numeric($lat)) {
+        die(json_encode(array("error" => "latitude incorrectly defined")));
+    } elseif (!is_numeric($long)) {
+        die(json_encode(array("error" => "longitude incorrectly defined")));
+    } elseif (!is_numeric($amount)) {
+        die(json_encode(array("error" => "amount incorrectly defined")));
+    } elseif (!is_numeric($weight)) {
+        die(json_encode(array("error" => "weight incorrectly defined")));
     }
 
-    if (execQuery($sql)) {
+    if(isset($_POST["image"])) {
+        $target_dir = "images/";
+        $GUID = GUIDv4();
+        $imagedir = $target_dir . $GUID;
+        $uploadOk = 1;
+        $imageFileType = pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION);
+
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        if (file_exists($imagedir)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $imagedir)) {
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+
+    if ($database->addNewFoodItem($name,$expirDate,$category,$userID,$desc,$lat,$long,$amount,$weight,$imagedir)) {
         $toEncode = array("success" => "topic added");
     } else {
         $toEncode = array("error" => "failed to add");
--- GPMain/includes.php
+++ GPMain/includes.php
@@ -6,6 +6,7 @@ define('ROOT', dirname(__FILE__));
 define('LOGIN', "localhost:8080/GroupProject/index.html");
 
 require_once (ROOT."/backend/database.php");
+require_once (ROOT."/backend/authentication.php");
 
 // Named autoloader
 function autoload_class($class){
